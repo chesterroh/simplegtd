8d1d73d6b29e3a51d54bef00ed21648042d18c95
Chester Jungseok Roh <chester.roh@gmail.com>
2017-10-01 20:03:27 +0900

    pyplot tried

--- polls/views.py
+++ polls/views.py
@@ -1,29 +1,32 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
-from django.template import loader
-from django.http import Http404
-from django.urls import reverse 
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from django.views import generic as djangogeneric
+
+from matplotlib import pylab 
+from pylab import *
+import PIL, PIL.Image
+from io import StringIO 
 
 # Create your views here.
 
 from django.http import HttpResponse
 from .models import Question,Choice
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = { 'latest_question_list' : latest_question_list, }
-    return render(request,'polls/index.html',context)
+class IndexView(djangogeneric.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request,question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist!")
-    return render(request,'polls/detail.html',{ 'question': question})
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
-def results(request,question_id):
-    question = get_object_or_404(Question,pk=question_id)
-    return render(request,'polls/results.html', { 'question' : question })
+class DetailView(djangogeneric.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultView(djangogeneric.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request,question_id):
     question = get_object_or_404(Question,pk=question_id)
@@ -39,8 +42,25 @@ def vote(request,question_id):
         selected_choice.save()
 
         return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
-    
             
-        
+def showimage(request):
+    t = arange(0.0,2.0,0.01)
+    s = sin(2*pi*t)
+    plot(t,s,linewidth=1.0)
 
+    xlabel('time (s)')
+    ylabel('voltage (mV)')
+    title('This is the test graph')
+    grid(True)
+
+    buffer = StringIO()
+    canvas = pylab.get_current_fig_manager().canvas
+    canvas.draw()
+    
+    pilImage = PIL.Image.frombytes("RGB", canvas.get_width_height(), canvas.tostring_rgb())
+    pilImage.save(buffer, "PNG")
+    pylab.close()
+ 
+    # Send buffer in a http response the the browser with the mime type image/png set
+    return HttpResponse(buffer.getvalue(), mimetype="image/png")
 
