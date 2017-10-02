a745a6ef2d25a1bb064f9ad3f15e2851efdfd166
Chester Jungseok Roh <chester.roh@gmail.com>
2017-10-02 17:05:28 +0900

    unnecessary data deleted

--- polls/views.py
+++ polls/views.py
@@ -70,15 +70,12 @@ def logaqi(request):
     for i in request.GET:
         query_string.append(request.GET[i])
     
-    temp = float(request.GET['temp'])
-    humid = float(request.GET['humid'])
     aqi01 = float(request.GET['aqi01'])
     aqi25 = float(request.GET['aqi25'])
     aqi10 = float(request.GET['aqi10'])
     voc = float(request.GET['voc'])
-    co2 = float(request.GET['co2'])
 
-    return HttpResponse("%s Temp %f / Humidity %f / AQI 1.0 %f / AQI 2_5 %f / AQI 10 %f / VOC %f / CO2 %f" % ( query_string, temp, humid, aqi01, aqi25, aqi10, voc, co2) )
+    return HttpResponse("%s / AQI 1.0 %f / AQI 2_5 %f / AQI 10 %f / VOC %f /" % ( query_string, aqi01, aqi25, aqi10, voc ) )
 
     
     
