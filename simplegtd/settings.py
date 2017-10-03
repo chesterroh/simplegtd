cc232189afa4c023487de11ca8cb89adeae14cf6
Chester Jungseok Roh <chester.roh@gmail.com>
2017-10-03 18:21:23 +0900

    logaqi app added

--- simplegtd/settings.py
+++ simplegtd/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'logaqi.apps.LogaqiConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
